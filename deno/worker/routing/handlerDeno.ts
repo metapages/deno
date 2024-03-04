dc4371faf62ec536a56e8a6af38d5fbc1895a1f9
Dion Amago <dion@transition9.com>
2024-03-04 14:44:27 -0800

    Slightly better handler

--- deno/worker/routing/handlerDeno.ts
+++ deno/worker/routing/handlerDeno.ts
@@ -2,50 +2,45 @@
  * Create request handler (incl websockets) for Deno Deploy
  */
 export const createHandler = (
-    handlerHttp: (request: Request) => Promise<Response | undefined>,
-    handlerWs?: (socket: WebSocket, request: Request) => void
-  ) => {
-    const boundHandlerWs = handlerWs ? createHandlerWs(handlerWs) : undefined;
-  
-    return async (request: Request): Promise<Response> => {
-      try {
-        let response: Response | undefined;
-        if (boundHandlerWs) { 
-          response = boundHandlerWs(request);
-          if (response) {
-            return response;
-          }
-        }
-  
-        response = await handlerHttp(request);
+  handlerHttp: (request: Request) => Promise<Response | undefined>,
+  handlerWs?: (socket: WebSocket, request: Request) => void
+) => {
+  const boundHandlerWs = handlerWs ? createHandlerWs(handlerWs) : undefined;
+
+  return async (request: Request): Promise<Response> => {
+    try {
+      let response: Response | undefined;
+      if (boundHandlerWs && request.headers.get("Upgrade") === "websocket") { 
+        response = boundHandlerWs(request);
         if (response) {
           return response;
         }
-        return new Response("i am not touching that", { status: 400 });
-      } catch (e) {
-        console.error(e.message);
-        return new Response("i fell over", { status: 500 });
       }
-    };
-  };
-  
-  const createHandlerWs = (handler: (socket: WebSocket,
-    request: Request) => void) => (request: Request): Response | undefined => {
-    const upgrade = request.headers.get("Upgrade");
-    if (upgrade === "websocket") {
-      try {
-  
-        const { socket, response } = Deno.upgradeWebSocket(request);
-  
-        socket.addEventListener("open", () => {
-          handler(socket, request);
-        });
-  
+
+      response = await handlerHttp(request);
+      if (response) {
         return response;
-      } catch (e) {
-        console.error(e);
-        return new Response(e.message, { status: 500 });
       }
+      return new Response("i am not touching that", { status: 400 });
+    } catch (e) {
+      console.error(e.message);
+      return new Response("i fell over", { status: 500 });
     }
   };
-  
+};
+
+const createHandlerWs = (handler: (socket: WebSocket,
+  request: Request) => void) => (request: Request): Response | undefined => {
+  try {
+    const { socket, response } = Deno.upgradeWebSocket(request);
+
+    socket.addEventListener("open", () => {
+      handler(socket, request);
+    });
+
+    return response;
+  } catch (e) {
+    console.error(e);
+    return new Response(e.message, { status: 500 });
+  }
+};
